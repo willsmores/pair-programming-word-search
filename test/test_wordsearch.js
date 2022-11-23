70859e6932169dcbaddff36684351cbeb4678813
Steve Willsmore <scwillsmore@gmail.com>
2022-11-23 17:22:29 -0500

    Updated to pass mocha tests.

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -32,7 +32,38 @@ describe("#wordSearch()", function() {
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
     ], 'SEINFELD')
+  
+    assert.isTrue(result);
+  })
+  //J
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'A', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'T', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'E', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'N', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FASTEN')
 
     assert.isTrue(result);
-  });
+  })
+    //Steve
+    it("should return false if a string is not entered", function() {
+      const result = wordSearch([
+        ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+        ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+        ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+        ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+        ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+        ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+        ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+        ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+        ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+      ], '')
+    
+    assert.isFalse(result)})
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,56 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+// Pair programming by @willsmores and @wayandandae
+
+// transpose a 2D array diagonally, then return an array with its width and length switched
+const transpose = matrix => {
+// create a variable to hold the resulting array
+const result = [];
+// error message for different array length than the first array
+const lError = "Every array's length should match the length of the first array. Please try again.";
+// number of elements of the first array
+const matrixL = matrix[0].length;
+const matrixH = matrix.length
+
+// // iterate through each array
+// matrix.forEach(array => {
+//   // if the length of another array is different than matrixL, throw the error message
+//   if (array.length !== matrixL) throw new Error(lError);
+// })
+
+// iterate through each array, until width of array is reached
+for(let i = 0; i < matrixL; i++){
+    // create a varaible to hold each line of array
+    result[i] = [];
+    // iterate through each element of array, until height of array
+    for(let j = 0; j < matrixH; j++){
+    // push element of same index from each array
+    result[i].push(matrix[j][i])
     }
 }
+
+// return the resulting transposed array
+return result;
+};
+
+
+const wordSearch = (letters, word) => { 
+
+let wordFound = false;
+if (word === "") return false;
+const horizontalJoin = letters.map(ls => ls.join(''));
+const horizontalJoinBw = letters.map(ls => ls.reverse().join(''));
+const verticalJoin = transpose(letters).map(ls => ls.join(''));
+const verticalJoinBw = transpose(letters).map(ls => ls.reverse().join(''));
+const directions = [horizontalJoin, horizontalJoinBw , verticalJoin, verticalJoinBw]
+
+console.log(directions)
+directions.forEach(direction => {
+    for (line of direction) {
+    if (line.includes(word)) wordFound = true;
+    }
+})
+
+
+return wordFound;
+};
 
 module.exports = wordSearch
